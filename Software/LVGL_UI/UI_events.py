3235240f1cf33f9df716a9c4824eaf195fafde74
Dang Vi Tin <dangvitin@gmail.com>
2024-12-15 17:37:27 +0700

    SquareLine Studio project

---
+++ Software/LVGL_UI/UI_events.py
@@ -0,0 +1,72 @@
+
+def asdsadasd(event_struct):
+    return
+
+
+def nextObj(event_struct):
+    return
+
+
+def nextObj1(event_struct):
+    return
+
+
+def nextObj2(event_struct):
+    return
+
+
+def updateValue(event_struct):
+    return
+
+
+def set_input_screen_1(event_struct):
+    return
+
+
+def set_input_screen_2(event_struct):
+    return
+
+
+def set_input_screen_3(event_struct):
+    return
+
+
+def effects_change(event_struct):
+    return
+
+
+def screen_3_init_effect(event_struct):
+    return
+
+
+def screen_3_exit_effect(event_struct):
+    return
+
+
+def screen_1_start_load(event_struct):
+    return
+
+
+def screen_2_start_load(event_struct):
+    return
+
+
+def screen_3_start_load(event_struct):
+    return
+
+
+def screen_3_start_unload(event_struct):
+    return
+
+
+def scr3_roller_change(event_struct):
+    return
+
+
+def scr3_roller_key_left(event_struct):
+    return
+
+
+def scr3_roller_key_right(event_struct):
+    return
+
